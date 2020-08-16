277917f929a787021c608325600f2daf238b2386
shizuka <shizuka.10th.august@icloud.com>
2020-08-16 14:18:04 +0900

    Initial Commit

---
+++ TitechAppLite/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TitechAppLite
+//
+//  Created by 柳田 涼華 on 2020/08/16.
+//  Copyright © 2020 Lei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
